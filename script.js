532a25485b09a3f287343f3cb0e75195824b5900
Sofia <sofia.souzz.alves@gmail.com>
2026-09-20 21:09:47 -0300

    Adiciona exclusão por ID

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function cadastrarProduto(){
     }
     mercadoria.push(produto)
 }
+
+function DelporID(){
+    const idProcurado = prompt("Qual é o id do produto que você quer remover?")
+    const Indexitem = mercadoria.findIndex(a => a.id == idProcurado)
+
+    if (Indexitem != -1){
+        alunos.splice(Indexitem, 1)
+        alert("Item removido com sucesso")
+    } else{
+        alert("Item não encontrado.")
+    }
+}
+
+function {}
