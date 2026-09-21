06027b61122dd3aaac3635a4ddd73b8faadff0cc
Sofia <sofia.souzz.alves@gmail.com>
2026-09-20 22:32:34 -0300

    cadastro de produtos atualizada, deletar pr id atualizada, mostrar produtos atualizada e botões inseridos no html pra teste

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 const mercadoria = []
 
 function cadastrarProduto(){
+    const nome = prompt("Insira o nome do produto")
+    if(nome === null || nome === ""){
+        alert("O produto não pôde ser cadastrado. O nome do produto é obrigatório")
+        return}
+
+    const marca = prompt("Insira a marca do produto")
+    if(marca === null || marca === ""){
+        alert("O produto não pôde ser cadastrado. A marca do produto é obrigatório")
+        return
+    }
+    
     const produto = {
         id: mercadoria.length + 1,
-        nome: prompt("Insira o nome do produto"),
-        marca: prompt("Insira a marca do produto")
+        nome: nome,
+        marca: marca
     }
-    mercadoria.push(produto)
+        mercadoria.push(produto)
 }
 
 function DelporID(){
+    if(mercadoria.length == 0){
+    alert("Não há produtos cadastrados para que possa deletá-los.")
+    return}
     const idProcurado = prompt("Qual é o id do produto que você quer remover?")
     const Indexproduto = mercadoria.findIndex(a => a.id == idProcurado)
 
     if (Indexproduto != -1){
-        alunos.splice(Indexproduto, 1)
+        mercadoria.splice(Indexproduto, 1)
         alert("Produto removido com sucesso")
     } else{
         alert("Produto não encontrado.")
     }
+  
 }
 
 function mostrarProdutos(){
     if (mercadoria.length > 0) {
+        let produtos = `=== PRODUTOS ===\n`
         for (let i = 0; i < mercadoria.length; i++) {
-            alert(`=== PRODUTOS ===\n
-                id: ${mercadorias[i].id}\n
-                nome: ${mercadorias[i].nome}\n
-                marca: ${mercadorias[i].marca}\n
-                ===============================\n`) }
+            produtos = produtos + `id: ${mercadoria[i].id}\n`
+            produtos = produtos + `nome: ${mercadoria[i].nome}\n` 
+            produtos = produtos + `marca: ${mercadoria[i].marca}\n`
+            produtos = produtos + `===============================\n`
+        }
+        alert(produtos)
     } else{
         alert("Nenhum produto cadastrado")
-    }
-    
+    }   
 }
